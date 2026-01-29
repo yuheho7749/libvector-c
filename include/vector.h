7233810bb872757646f9dfd2dbb75019af38c247
yuheho7749 <yuheho7749@gmail.com>
2026-01-28 18:28:59 -0800

    clean up compiler detection for compiler specific optimizations

--- include/vector.h
+++ include/vector.h
@@ -4,15 +4,25 @@
 #include <stdint.h>
 #include <stddef.h>
 
+// --- CLANG ---
 #if defined(__clang__)
+#define CLANG_COMPILER
+#define no_optimize __attribute__((optnone))
+
+// --- GNUC (assumes gcc flags/attributes as "standard") ---
 #elif defined(__GNUC__)
+#define GNUC_COMPILER
+#define no_optimize __attribute__((optimize("O0")))
+
+// --- Other compilers ---
 #else
 #error "Unsupported compiler"
 #endif
 
+
 #ifndef VECTOR_GROW_FACTOR
 #define VECTOR_GROW_FACTOR 1.5f
-#endif
+#endif // Compiler check end
 
 typedef struct {
     size_t size;
@@ -21,7 +31,7 @@ typedef struct {
     void* data;
 } vector_t;
 
-// User handles alloc/free of the vector struct
+// User handles alloc/free of the vector struct (but not the data field)
 int vector_init(vector_t *vec, size_t datatype_bytes, size_t init_capacity);
 void vector_free(vector_t *vec);
 int vector_recast_datatype(vector_t *vec, size_t new_datatype_bytes);
@@ -29,6 +39,7 @@ int vector_recast_datatype(vector_t *vec, size_t new_datatype_bytes);
 int vector_append(vector_t *vec, const void *elem);
 int vector_prepend(vector_t *vec, const void *elem);
 int vector_insert(vector_t *vec, const void *elem, size_t index); // TODO:
+int vector_remove(vector_t *vec, size_t index); // TODO:
 
 void *vector_get(const vector_t *vec, size_t index);
 int vector_resize(vector_t *vec, size_t new_size);
